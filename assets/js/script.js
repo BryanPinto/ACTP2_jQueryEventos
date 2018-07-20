71fcce0666d4a1ee8c311660826375bf7229e07b
Bryan Pinto <bryaan.pintoo@gmail.com>
2018-07-19 20:59:59 -0400

    se muestra imagen seleccionada en un select

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,15 @@ $(document).ready(function(){
 	$(".card__like").on("click", function(e){
 		$(this).toggleClass("card__like--red");
 		e.stopPropagation();
-	});
-	//evitar que un link me navegue a #
-	$("a[href='#']").on("click", function(e){
 		e.preventDefault();
 	});
 	//agregar y quitar clase a boton seguir de las cartas
 	$(".card__follow-btn").on("click", function(e){
 		$(this).toggleClass("card__follow-btn--following");
 		e.stopPropagation();
+	});
+	//mostrar imagen al seleccionarla de la lista despleglable
+	$("#image").on("change", function(){
+		$(".create__image img").attr("src", "assets/images/squared/" + $(this).val());
 	});
 });
