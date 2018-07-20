d38129558c567f49fcc2bb86849850f9788df8c7
Bryan Pinto <bryaan.pintoo@gmail.com>
2018-07-19 20:28:55 -0400

    card__like con toggle class card__like--red

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-	//el codigo va aqui
+	//abrir y cerrar cartas (imagenes)
 	$(".card").on("click", function(){
 		$(this).toggleClass("card--open");
-	})
+	});
+	//agregar y quitar color rojo a corazones
+	$(".card__like").on("click", function(e){
+		$(this).toggleClass("card__like--red");
+		e.stopPropagation();
+	});
+	//evitar que un link me navegue a #
+	$("a[href='#']").on("click", function(e){
+		e.preventDefault();
+	});
 });
